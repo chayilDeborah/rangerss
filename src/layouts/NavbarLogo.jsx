89d6147dfe0741fcdb8e7ed8ad1538a2244fda51
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-08-11 09:42:32 +0100

    commit

--- src/layouts/NavbarLogo.jsx
+++ src/layouts/NavbarLogo.jsx
@@ -1,5 +1,7 @@
 import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
+// import '../styles/ui/navbar.scss';
+// import { dropdown-container } from '../styles/ui/navbar'
 
 const NavbarLogo = () => {
     return (
--- src/layouts/dashboard/Sidebar.jsx
+++ src/layouts/dashboard/Sidebar.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import { SideBarDataOne } from "../../data/Dropdown/data";
 import { SideBarDataTwo } from "../../data/Dropdown/data";
+import '../../styles/layout/sidebar.scss';
+
 
 const DashboardSideBar = () => {
   return (
